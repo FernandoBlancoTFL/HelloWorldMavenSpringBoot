cffa97962395bee9d045b9455afa0ffe7daa6c4b
TilFerLix <Fernando.blanco004@gmail.com>
2024-09-06 22:28:14 -0300

    Initial commit

---
+++ src/main/java/org/example/helloworld/helloworldmaven/HelloWorldMavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.helloworld.helloworldmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/helloworld/helloworldmaven/HelloWorldMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.helloworld.helloworldmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
